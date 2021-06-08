6b9d059a0f8cd2f85ff7b71a09c02f7214f507fc
marcos <toledompm@gmail.com>
2021-06-08 20:48:41 -0300

    refactor: login

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div id="nav" class="row text-center">
       <router-link to="/">Home</router-link> |
       <router-link to="/anotacoes">Anotações</router-link> |
-      <a v-if="usuario" @click="logout">Logout</a>
-      <router-link v-if="!usuario" to="/login">Login</router-link>
+      <a v-if="user" @click="logout">Logout</a>
+      <router-link v-if="!user" to="/login">Login</router-link>
     </div>
     <router-view />
   </div>
@@ -17,7 +17,7 @@ import { mapMutations } from 'vuex';
 export default {
   name: 'app',
   computed: {
-    ...mapState(['usuario']),
+    ...mapState(['user']),
   },
   methods: {
     ...mapMutations(['logout']),
